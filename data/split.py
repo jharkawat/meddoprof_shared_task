c06b1224d69fcd2ed9d68b0bc00c49b8d4ef8fde
jharkawat <jalaj.iitkgp@gmail.com>
2021-06-07 20:26:23 +0530

    seperate files

--- data/split.py
+++ data/split.py
@@ -1,21 +1,19 @@
-file = open("train.txt", "r")
+file = open("test.txt", "r")
 lines = file.readlines()
 count = 0
 i = 0
+smallfile = None
 # smallfile = open("train.txt","w")
 for line in lines:
     if line == "\n":
         count += 1
-    i += 1
-    # if i == 697157:
-    #     smallfile.close()
-    #     smallfile = open("dev.txt","w")
-    # if i == 935652:
-    #     smallfile.close()
-    #     smallfile = open("test.txt", "w")
-    # smallfile.write(line)
-print(i)
-print(count)
+        if smallfile:
+            smallfile.close()
+        small_filename = 'test/small_file_test_{}.txt'.format(count)
+        smallfile = open(small_filename, "w")
+    smallfile.write(line)
+if smallfile:
+    smallfile.close()
 file.close()
 
 
